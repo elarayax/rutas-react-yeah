c2f243cb06726d45ce107ffb63acc9719893cee7
elarayax <37583319+elarayax@users.noreply.github.com>
2025-09-29 10:15:48 -0300

    Proyecto inicial con rutas dinámicas, Atomic Design y LocalStorage

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,7 @@
+import { Button as BootstrapButton } from 'react-bootstrap';
+
+function Button({ children, ...props }) {
+  return <BootstrapButton {...props}>{children}</BootstrapButton>;
+}
+
+export default Button;
---
+++ src/components/atoms/Text.jsx
@@ -0,0 +1,6 @@
+function Text({ children, variant = 'p', className }) {
+  const Tag = variant; // Puede ser 'h1', 'p', 'span', etc.
+  return <Tag className={className}>{children}</Tag>;
+}
+
+export default Text;
